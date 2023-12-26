3d3eb2f8054399174190d3248e4e22c564ea47a4
Iamnoob77 <68275507+Iamnoob77@users.noreply.github.com>
2023-12-26 14:48:03 +0545

    installed package

--- frontend/src/redux/homeSlice.jsx
+++ frontend/src/redux/homeSlice.jsx
@@ -1,4 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
 import axios from "axios";
 import { BASE_URL } from "src/config/base_url";
 
